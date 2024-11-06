373ab69db93ee28599c5fd0e8e2bfcbd9e541442
roy-scalis <155763567+roy-scalis@users.noreply.github.com>
2024-11-06 17:34:34 -0500

    added editor box

--- app/roy3/Page.tsx
+++ app/roy3/Page.tsx
@@ -5,11 +5,14 @@ import Chats, { ChatsProvider, ChatsProps } from "./Chats";
 export default function Page() {
   return (
     <div className="page-container">
-      <div className="page-column">
+      <div className="page-column boxed">
         <Messages />
       </div>
-      <div className="page-column">
-        <Chats />
+      <div className="page-column boxed">
+        <div className="upper-section boxed">
+          <Chats />
+        </div>
+        <div className="lower-section boxed">editor goes here</div>
       </div>
     </div>
   );
