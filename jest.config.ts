fa19a02660dbb7592b7bcaeab3d4a0f767483374
roy-scalis <155763567+roy-scalis@users.noreply.github.com>
2024-11-12 12:14:43 -0500

    can now run jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ const customJestConfig = {
     "^~/(.*)$": "<rootDir>/src/$1",
     "^@graphql/core/(.*)$": "<rootDir>/src/core/$1",
   },
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest", // The important part right here
   },
